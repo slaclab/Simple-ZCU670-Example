f7e7d39559e21ddf6c21c2351bb8d3f0f043777d
Larry Ruckman <ruckman@slac.stanford.edu>
2024-07-05 12:29:23 -0700

    Update vivado_setup.sh

--- firmware/vivado_setup.sh
+++ firmware/vivado_setup.sh
@@ -1,2 +1,2 @@
 # Setup the Xilinx software and licensing
-source /afs/slac.stanford.edu/g/reseng/xilinx/2023.1/Vivado/2023.1/settings64.sh
+source /afs/slac.stanford.edu/g/reseng/xilinx/2024.1/Vivado/2024.1/settings64.sh
